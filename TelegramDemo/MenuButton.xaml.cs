8c188b9e0de757c5c550b28d530010f3cc99b09e
SobirjonovSodiqjonovich <otkirbeksobirjonov@gmail.com>
2022-02-16 16:33:23 +0500

    Add project files.

---
+++ TelegramDemo/MenuButton.xaml.cs
@@ -0,0 +1,54 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TelegramDemo
+{
+    /// <summary>
+    /// Interaction logic for MenuButton.xaml
+    /// </summary>
+    public partial class MenuButton : UserControl
+    {
+        public PackIconKind PackIconName { get; set; } = PackIconKind.ViewDashboard;
+        public string ButtonName { get; set; }
+
+        public MenuButton()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            BrushConverter bc = new BrushConverter();
+            ((Border)sender).Background = Brushes.White;
+            dIcon.Foreground = (Brush)bc.ConvertFrom("#209F84");
+            dashboardText.Foreground = (Brush)bc.ConvertFrom("#209F84");
+        }
+
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            BrushConverter bc = new BrushConverter();
+            ((Border)sender).Background = (Brush) bc.ConvertFrom("#209F84");
+            dIcon.Foreground = Brushes.White;
+            dashboardText.Foreground = Brushes.White;
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            dashboardText.Content = ButtonName;
+            dIcon.Kind = PackIconName;
+        }
+    }
+}
